e49393ac3f966d16e8976b2856bd1f9417a84b5f
michallef <142462683+michallef@users.noreply.github.com>
2023-08-22 21:21:16 -0700

    Create main.py

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ def main():
     ov_rate = 27.78
 
    ##################################################
-   # Code your program here
+    overtime = workhours - reg_hours 
+    overtime_wage = overtime * ov_rate
+    regular_wage = reg_hours * reg_rate
+    total_wage = regular_wage + overtime_wage 
    ##################################################
     # overtime = workhours - reg_hours
     # overtime_wage = overtime * ov_rate
     # regular_wage = reg_hours * reg_rate
     # total_wage = regular_wage + overtime_wage
 
-    print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
-    print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
+    print(f"Regular hours: {reg_hours}      Regular Charge: {regular_wage}")
+    print(f"Overtime hours: {overtime}     Overtime Charge: {overtime_wage:.2f}")
     print(f"Total wage : {total_wage:.2f}")
 
    ##################################################
